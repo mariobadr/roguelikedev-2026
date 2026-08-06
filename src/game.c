24174ed021325803a340f4cf51ac08b863ce4e9b
Mario Badr <mario.badr@utoronto.ca>
2026-08-05 20:19:48 -0400

    Add mouse-click-to-move functionality

--- src/game.c
+++ src/game.c
@@ -4,12 +4,96 @@
 
 #include "input.h"
 
+#define TILE_SIZE (8.0f)
+
+/**
+ * Return the sign of number.
+ *
+ * @param number The value to check.
+ *
+ * @return -1 for negative, 1 for positive, and 0 for zero
+ */
+static int
+sign(int number)
+{
+  return (number > 0) - (number < 0);
+}
+
+/**
+ * Update action to a move action with (x, y) as the vector.
+ *
+ * @param action the action to mutate
+ * @param x      the x-component of the vector
+ * @param y      the y-component of the vector
+ */
 static void
 set_movement_action(struct rl_action* action, int x, int y)
 {
   action->type = RL_ACTION_MOVE;
   action->move_vector.x = x;
   action->move_vector.y = y;
+}
+
+/**
+ * Update action based on any pressed keys in istate.
+ *
+ * @param action the action to mutate
+ * @param istate the current frame's input state
+ *
+ * @return whether action was mutated
+ */
+static bool
+handle_keyboard_input(struct rl_action* action, struct inpt_state const* istate)
+{
+  if (inpt_is_down(istate->keys[SDL_SCANCODE_W])) {
+    set_movement_action(action, 0, -1);
+    return true;
+  }
+
+  if (inpt_is_down(istate->keys[SDL_SCANCODE_S])) {
+    set_movement_action(action, 0, 1);
+    return true;
+  }
+
+  if (inpt_is_down(istate->keys[SDL_SCANCODE_A])) {
+    set_movement_action(action, -1, 0);
+    return true;
+  }
+
+  if (inpt_is_down(istate->keys[SDL_SCANCODE_D])) {
+    set_movement_action(action, 1, 0);
+    return true;
+  }
+
+  return false;
+}
+
+static bool
+handle_mouse_input(struct rl_game* game, struct inpt_state const* istate)
+{
+  if (inpt_is_down(istate->mouse.buttons[SDL_BUTTON_LEFT])) {
+    int const target_x = (int)SDL_floorf(istate->mouse.position.x / TILE_SIZE);
+    int const target_y = (int)SDL_floorf(istate->mouse.position.y / TILE_SIZE);
+
+    int const delta_x = target_x - game->player.x;
+    int const delta_y = target_y - game->player.y;
+
+    if (delta_x == 0 && delta_y == 0) {
+      // already at target
+      return false;
+    }
+
+    // move along x- or y-axis, but not both
+    if (SDL_abs(delta_x) > SDL_abs(delta_y)) {
+      set_movement_action(&game->action, sign(delta_x), 0);
+    } else {
+      set_movement_action(&game->action, 0, sign(delta_y));
+    }
+
+    return true;
+  }
+
+  return false;
 }
 
 bool
@@ -34,15 +118,12 @@ rl_handle_input(struct rl_game* game, struct inpt_state const* istate)
     return true;
   }
 
-  if (inpt_is_down(istate->keys[SDL_SCANCODE_W])) {
-    set_movement_action(&game->action, 0, -1);
-  } else if (inpt_is_down(istate->keys[SDL_SCANCODE_S])) {
-    set_movement_action(&game->action, 0, 1);
-  } else if (inpt_is_down(istate->keys[SDL_SCANCODE_A])) {
-    set_movement_action(&game->action, -1, 0);
-  } else if (inpt_is_down(istate->keys[SDL_SCANCODE_D])) {
-    set_movement_action(&game->action, 1, 0);
+  // prioritize keyboard input over mouse input (?)
+  if (handle_keyboard_input(&game->action, istate)) {
+    return true;
   }
+
+  handle_mouse_input(game, istate);
 
   return true;
 }
@@ -68,8 +149,8 @@ rl_render_game(struct rl_game* game, SDL_Renderer* renderer)
   SDL_RenderClear(renderer);
 
   // convert from tile to screen coordinates
-  float const player_x = game->player.x * 8.0f;
-  float const player_y = game->player.y * 8.0f;
+  float const player_x = game->player.x * TILE_SIZE;
+  float const player_y = game->player.y * TILE_SIZE;
 
   // draw the rogue
   SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
