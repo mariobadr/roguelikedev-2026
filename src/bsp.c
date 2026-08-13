ffcb6b42e46d47607b0353169381713407b58cdd
Mario Badr <mario.badr@utoronto.ca>
2026-08-13 16:40:07 -0400

    Add a BSP data structure and split algorithm

---
+++ src/bsp.c
@@ -0,0 +1,95 @@
+#include "bsp.h"
+
+#include "rand.h"
+
+void
+rl_bsp_tree_init(struct rl_bsp_tree* tree, SDL_Rect rect)
+{
+  SDL_memset(tree, 0, sizeof(*tree));
+  tree->nodes[0].rect = rect;
+}
+
+void
+rl_bsp_split(struct rl_bsp_tree* tree,
+             int index,
+             struct rand_state* rng,
+             int depth)
+{
+  if (depth >= RL_BSP_MAX_DEPTH) {
+    // cannot exceed the maximum depth
+    return;
+  }
+
+  struct rl_bsp_node* node = &tree->nodes[index];
+
+  // Check whether we can even split the node further
+  bool can_split_x = node->rect.w >= 2 * RL_BSP_MIN_SIZE;
+  bool can_split_y = node->rect.h >= 2 * RL_BSP_MIN_SIZE;
+  if (!can_split_x && !can_split_y) {
+    return;
+  }
+
+  // Determine how this node will be split
+  if (can_split_x && can_split_y) {
+    // Can split either way; decide based on width:height ratio
+    if (node->rect.w > node->rect.h) {
+      node->axis = RL_BSP_SPLIT_X;
+    } else if (node->rect.h > node->rect.w) {
+      node->axis = RL_BSP_SPLIT_Y;
+    } else {
+      node->axis = rand_next_between(rng, RL_BSP_SPLIT_X, RL_BSP_SPLIT_Y);
+    }
+  } else if (can_split_x) {
+    // Can only split on the x-axis
+    node->axis = RL_BSP_SPLIT_X;
+  } else {
+    // Can only split on the y-axis
+    node->axis = RL_BSP_SPLIT_Y;
+  }
+
+  // Do the splitting
+  struct rl_bsp_node* left = &tree->nodes[rl_bsp_left_of(index)];
+  struct rl_bsp_node* right = &tree->nodes[rl_bsp_right_of(index)];
+
+  if (node->axis == RL_BSP_SPLIT_X) {
+    // splitting here should not impact the height
+    int const split =
+      (int)rand_next_between(rng,
+                             node->rect.x + RL_BSP_MIN_SIZE,
+                             node->rect.x + node->rect.w - RL_BSP_MIN_SIZE);
+
+    // left child starts where parent started
+    left->rect.x = node->rect.x;
+    left->rect.y = node->rect.y;
+    left->rect.w = split - node->rect.x;
+    left->rect.h = node->rect.h;
+
+    // right child starts at the split line
+    right->rect.x = split;
+    right->rect.y = node->rect.y;
+    right->rect.w = (node->rect.x + node->rect.w) - split;
+    right->rect.h = node->rect.h;
+  } else {
+    // splitting here should not impact the width
+    int const split =
+      (int)rand_next_between(rng,
+                             node->rect.y + RL_BSP_MIN_SIZE,
+                             node->rect.y + node->rect.h - RL_BSP_MIN_SIZE);
+
+    // left child starts where parent started
+    left->rect.x = node->rect.x;
+    left->rect.y = node->rect.y;
+    left->rect.w = node->rect.w;
+    left->rect.h = split - node->rect.y;
+
+    // right child starts at the split line
+    right->rect.x = node->rect.x;
+    right->rect.y = split;
+    right->rect.w = node->rect.w;
+    right->rect.h = (node->rect.y + node->rect.h) - split;
+  }
+
+  // Continue splitting
+  rl_bsp_split(tree, rl_bsp_left_of(index), rng, depth + 1);
+  rl_bsp_split(tree, rl_bsp_right_of(index), rng, depth + 1);
+}
---
+++ src/bsp.h
@@ -0,0 +1,99 @@
+/**
+ * @file bsp.h
+ */
+#ifndef GINC_ROGUELIKE_BSP_H
+#define GINC_ROGUELIKE_BSP_H
+
+#include <SDL3/SDL_rect.h>
+#include <SDL3/SDL_stdinc.h>
+
+// forward declarations
+struct rand_state;
+
+/** The minimum size of a rect in the BSP tree. */
+#define RL_BSP_MIN_SIZE 5
+/** The maximum depth of the BSP tree. */
+#define RL_BSP_MAX_DEPTH 8
+/** The number of nodes needed for a BSP of maximum depth. */
+#define RL_BSP_CAPACITY ((1 << (RL_BSP_MAX_DEPTH + 1)) - 1)
+
+enum rl_bsp_split_axis
+{
+  RL_BSP_SPLIT_NONE, //< Leaf node
+  RL_BSP_SPLIT_X,    //< Node split on x-axis
+  RL_BSP_SPLIT_Y     //< Node split on y-axis
+};
+
+/**
+ * A node in a BSP tree.
+ */
+struct rl_bsp_node
+{
+  /** The region of space this node occupies. */
+  SDL_Rect rect;
+  /** How this node was split, if at all. */
+  enum rl_bsp_split_axis axis;
+};
+
+/**
+ * A BSP tree.
+ */
+struct rl_bsp_tree
+{
+  /** All nodes in the tree. */
+  struct rl_bsp_node nodes[RL_BSP_CAPACITY];
+};
+
+/**
+ * @return the index of the left child of the node at index.
+ */
+static inline int
+rl_bsp_left_of(int index)
+{
+  return 2 * index + 1;
+}
+
+/**
+ * @return the index of the right child of the node at index.
+ */
+static inline int
+rl_bsp_right_of(int index)
+{
+  return 2 * index + 2;
+}
+
+/**
+ * @return the index of the parent of the node at index.
+ */
+static inline int
+rl_bsp_parent_of(int index)
+{
+  return (index - 1) / 2;
+}
+
+/**
+ * Initializes a tree with the given root region.
+ *
+ * All nodes start as leaves (i.e., RL_BSP_SPLIT_NONE) until split.
+ *
+ * @param tree The tree to initialize.
+ * @param rect The root region that will be split.
+ */
+void
+rl_bsp_tree_init(struct rl_bsp_tree* tree, SDL_Rect rect);
+
+/**
+ * Split the node at index up to depth levels.
+ *
+ * @param tree  The tree being updated.
+ * @param index The index of the node of the tree.
+ * @param rng   The random number generator.
+ * @param depth The current depth.
+ */
+void
+rl_bsp_split(struct rl_bsp_tree* tree,
+             int index,
+             struct rand_state* rng,
+             int depth);
+
+#endif // GINC_ROGUELIKE_BSP_H
